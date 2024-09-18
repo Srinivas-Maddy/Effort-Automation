1870818e2cc12d763547293e0113340183319689
devarakondasaikiran <saikirandevarakonda9@gmail.com>
2024-09-18 10:22:11 +0530

    work filter 4 cases

--- src/test/java/com/effort/nxt/test/filters/WorkFiltersTest.java
+++ src/test/java/com/effort/nxt/test/filters/WorkFiltersTest.java
@@ -104,6 +104,7 @@ public class WorkFiltersTest extends BaseAutomationTest {
 		}
 	
 	
+	
 	@AfterClass(alwaysRun = true)
 	public void quitDriver() {
 		logger.info("Starting of quitDriver Method");
